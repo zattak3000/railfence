7c1eaf275d30e5da9ef90ca4fbdd86d73222b126
Zack W <71153497+zattak3000@users.noreply.github.com>
2021-11-13 22:48:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rail Fence Cipher
+A rail fence cipher I wrote in C in the middle of the night in about an hour
+
+## Usage
+```
+Enter plaintext: supersecret
+Enter key: 4
+Encrypted with key 4: seuscprrtee
+```
---
+++ rf.c
@@ -0,0 +1,92 @@
+//                  ||  '||    .'|.                                  
+// ... ..   ....   ...   ||  .||.     ....  .. ...     ....    ....  
+//  ||' '' '' .||   ||   ||   ||    .|...||  ||  ||  .|   '' .|...|| 
+//  ||     .|' ||   ||   ||   ||    ||       ||  ||  ||      ||      
+// .||.    '|..'|' .||. .||. .||.    '|...' .||. ||.  '|...'  '|...' .c
+//
+// Author: zattak3000
+// Last Update: 11/13/2021
+
+#include <stdio.h>
+#include <string.h>
+
+
+void encode(char *cipher, char *plaintext, int key)
+{
+    char fence[10][50];
+    char idx[10] = {};
+
+    char output[100];
+    int len = strlen(plaintext);
+
+    int i = 0;
+    char r = 0;
+    
+    while (i < len)
+    {   
+        for (; r < key; r++)
+        {
+            if (i >= len)
+            {
+                i = len + 1;
+                break;
+            }
+
+            fence[r][idx[r]++] = plaintext[i++];
+        }
+        r -= 2;
+
+        for (; r >= 0; r--)
+        {
+            if (i >= len) 
+            {
+                i = len + 1;
+                break;
+            }
+
+            fence[r][idx[r]++] = plaintext[i++];
+        }
+        r += 2;
+    }
+
+    char outidx = 0;
+
+    for (int i = 0; i < key; i++)
+    {
+        for (int j = 0; j < idx[i]; j++)
+        {
+            output[outidx++] = fence[i][j];   
+        }
+    }
+
+    output[outidx] = '\0';
+
+    strcpy(cipher, output);
+}
+
+void decode(char *plaintext, char *cipher, int key)
+{
+    // Not quite there yet...
+}
+
+int main(int argc, char const *argv[])
+{
+    // Recieve user input
+    char plaintext[50];
+    printf("Enter plaintext: ");
+    fgets(plaintext, sizeof(plaintext), stdin);
+    
+    int key;
+    printf("Enter key: ");
+    scanf("%u", &key);
+
+    // Strip newline
+    if (strchr(plaintext, '\n'))
+    {
+        *strchr(plaintext, '\n') = '\0';
+    }
+
+    char output[50];
+    encode(output, plaintext, key);
+    printf("Encrypted with key %d: %s\n", key, output);
+}
